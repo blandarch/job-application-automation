f53ef47047bb1a75f8ba0ed3d94148b0b354e660
Justine <justinevillarosa@gmail.com>
2024-02-22 16:21:36 +0800

    added initial negative test cases for log in

--- selenium_automation_framework/test/test_linkedin_actions.py
+++ selenium_automation_framework/test/test_linkedin_actions.py
@@ -22,3 +22,45 @@ def test_linkedin_login():
         linkedin_actions.properties.login_successful_indicator
         in linkedin_actions.properties.driver.title
     )
+
+
+def test_linkedin_login_wrong_username():
+    """_summary_: method to test login with wrong username and right password"""
+
+    # sets up driver and linkedIn actions instance
+    driver = chromedriver_setup()
+    linkedin_actions = LinkedInActions(driver)
+    # sets up wrong username and right password
+    wrong_username = "abc@gmail.com"
+    password = linkedin_actions.properties.get_login_credentials()[1]
+
+    # logs in
+    linkedin_actions.go_to_login_page()
+    linkedin_actions.login(wrong_username, password)
+
+    # asserts that user is not able to login
+    assert (
+        linkedin_actions.properties.login_successful_indicator
+        not in linkedin_actions.properties.driver.title
+    )
+
+
+def test_linkedin_login_wrong_password():
+    """_summary_: method to test login with right username and wrong password"""
+
+    # sets up driver and linkedIn actions instance
+    driver = chromedriver_setup()
+    linkedin_actions = LinkedInActions(driver)
+    # sets up wrong username and right password
+    wrong_username = linkedin_actions.properties.get_login_credentials()[0]
+    password = "t3st1ngWronGP@assword"
+
+    # logs in
+    linkedin_actions.go_to_login_page()
+    linkedin_actions.login(wrong_username, password)
+
+    # asserts that user is not able to login
+    assert (
+        linkedin_actions.properties.login_successful_indicator
+        not in linkedin_actions.properties.driver.title
+    )
