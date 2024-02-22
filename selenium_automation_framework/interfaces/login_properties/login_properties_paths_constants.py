9ab279cda80ad060b5686f732f59e089e77c9629
Justine <justinevillarosa@gmail.com>
2024-02-22 16:39:11 +0800

    redirected json from different path

--- selenium_automation_framework/interfaces/login_properties/login_properties_paths_constants.py
+++ selenium_automation_framework/interfaces/login_properties/login_properties_paths_constants.py
@@ -1,3 +1,3 @@
 """Python File to specify what path to JSON files creds are located from login_properties"""
 
-LINKEDIN_CREDENTIALS_JSON = "credentials/linkedin.json"
+LINKEDIN_CREDENTIALS_JSON = "selenium_automation_framework/credentials/linkedin.json"
