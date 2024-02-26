8d883a16160753ea241d3ccf077ce144287a5d60
Justine <justinevillarosa@gmail.com>
2024-02-26 11:13:32 +0800

    fixed pylint errors in driver_setup.py

--- selenium_automation_framework/webdriver_setup/driver_setup.py
+++ selenium_automation_framework/webdriver_setup/driver_setup.py
@@ -2,11 +2,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-import chromedriver_autoinstaller
 from pyvirtualdisplay import Display
-
-# from webdriver_manager.chrome import ChromeDriverManager
 
 
 def chromedriver_setup():
@@ -23,12 +19,22 @@ def chromedriver_setup():
 
 
 def start_display():
+    """_summary_: method to use to start display and returns a Display instance
+
+    Returns:
+        Display: a started display to chromedriver display
+    """
     display = Display(visible=0, size=(800, 800))
     display.start()
     return display
 
 
 def get_webdriver_options():
+    """_summary_: returns a list of webdriver options to be passed to shell
+
+    Returns:
+        list: a list that contains a string of setup to be enabled for webdriver.
+    """
     return [
         # Define window size here
         "--window-size=1200,1200",
@@ -45,11 +51,12 @@ def get_webdriver_options():
 
 
 def add_chrome_options():
+    """_summary_: returns options to be used for chromedriver
+
+    Returns:
+        Options: an instance of Options where webdriver options are configured
+    """
     chrome_options = Options()
     for option in get_webdriver_options():
         chrome_options.add_argument(option)
     return chrome_options
-
-
-# def get_chrome_service():
-#     return Service(ChromeDriverManager(chrome_type=))
