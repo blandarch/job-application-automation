f2aafb841558bcd4ec9abf8cd20fbb127a9a52e9
Justine <justinevillarosa@gmail.com>
2024-02-26 10:58:01 +0800

    removed try/catch to test_excel_framework to increase test coverage

--- excel_framework/test/test_excel_framework.py
+++ excel_framework/test/test_excel_framework.py
@@ -9,10 +9,8 @@ from .static_constants import (
     DATA_TO_INPUT,
     SAVE_FILENAME,
     ADD_DATA_WITHOUT_FILENAME,
-    ADD_DATA_WITHOUT_FILENAME_ERROR,
     OPEN_FILENAME,
     EXPECTED_OUTPUT_FILENAME,
-    ADD_DATA_WITH_FILENAME_ERROR,
     ADD_DATA_EMPTY_ROW_CELL_ERROR,
     ADD_DATA_EMPTY_ROW_CELL,
 )
@@ -27,66 +25,41 @@ with open("excel_framework/test/data.json", encoding="utf-8") as file:
 def test_add_data_without_filename():
     """_summary_: checks validation on add_data using Without Filename"""
 
-    try:
-        # Adds data from empty excel file and saves it
-        TestHelpers.create_workbook(
-            data[ADD_DATA_WITHOUT_FILENAME][SHEET_NAME],
-            data[ADD_DATA_WITHOUT_FILENAME][DATA_TO_INPUT],
-            data[ADD_DATA_WITHOUT_FILENAME][SAVE_FILENAME],
-        )
+    # Adds data from empty excel file and saves it
+    TestHelpers.create_workbook(
+        data[ADD_DATA_WITHOUT_FILENAME][SHEET_NAME],
+        data[ADD_DATA_WITHOUT_FILENAME][DATA_TO_INPUT],
+        data[ADD_DATA_WITHOUT_FILENAME][SAVE_FILENAME],
+    )
 
-        # Asserts the produced excel file is equal to true
-        assert TestHelpers.validate_excel_files(
-            data[ADD_DATA_WITHOUT_FILENAME][SAVE_FILENAME],
-            CURRENT_DIRECTORY
-            + data[ADD_DATA_WITHOUT_FILENAME][EXPECTED_OUTPUT_FILENAME],
-        ) == (True, "Excel files are equal.")
+    # Asserts the produced excel file is equal to true
+    assert TestHelpers.validate_excel_files(
+        data[ADD_DATA_WITHOUT_FILENAME][SAVE_FILENAME],
+        CURRENT_DIRECTORY + data[ADD_DATA_WITHOUT_FILENAME][EXPECTED_OUTPUT_FILENAME],
+    ) == (True, "Excel files are equal.")
 
-    except FileNotFoundError as error:
-        print(ADD_DATA_WITHOUT_FILENAME_ERROR.replace(r"{0}", str(error)))
-        # Print the stack trace
-        traceback.print_exc()
-
-    except AssertionError as error:
-        print(ADD_DATA_WITHOUT_FILENAME_ERROR.replace(r"{0}", str(error)))
-        # Print the stack trace
-        traceback.print_exc()
-
-    finally:
-        # deletes sample file
-        TestHelpers.delete_file(data[ADD_DATA_WITHOUT_FILENAME][SAVE_FILENAME])
+    # deletes sample file
+    TestHelpers.delete_file(data[ADD_DATA_WITHOUT_FILENAME][SAVE_FILENAME])
 
 
 def test_add_data_with_existing_filename():
     """_summary_: checks validation on add_data using With Filename"""
-    try:
-        # Adds data from empty excel file and saves it
-        TestHelpers.create_workbook(
-            data[ADD_DATA_WITH_FILENAME][SHEET_NAME],
-            data[ADD_DATA_WITH_FILENAME][DATA_TO_INPUT],
-            data[ADD_DATA_WITH_FILENAME][SAVE_FILENAME],
-            CURRENT_DIRECTORY + data[ADD_DATA_WITH_FILENAME][OPEN_FILENAME],
-        )
+    # Adds data from empty excel file and saves it
+    TestHelpers.create_workbook(
+        data[ADD_DATA_WITH_FILENAME][SHEET_NAME],
+        data[ADD_DATA_WITH_FILENAME][DATA_TO_INPUT],
+        data[ADD_DATA_WITH_FILENAME][SAVE_FILENAME],
+        CURRENT_DIRECTORY + data[ADD_DATA_WITH_FILENAME][OPEN_FILENAME],
+    )
 
-        # Asserts the produced excel file is equal to true
-        assert TestHelpers.validate_excel_files(
-            data[ADD_DATA_WITH_FILENAME][SAVE_FILENAME],
-            CURRENT_DIRECTORY + data[ADD_DATA_WITH_FILENAME][EXPECTED_OUTPUT_FILENAME],
-        ) == (True, "Excel files are equal.")
+    # Asserts the produced excel file is equal to true
+    assert TestHelpers.validate_excel_files(
+        data[ADD_DATA_WITH_FILENAME][SAVE_FILENAME],
+        CURRENT_DIRECTORY + data[ADD_DATA_WITH_FILENAME][EXPECTED_OUTPUT_FILENAME],
+    ) == (True, "Excel files are equal.")
 
-    except FileNotFoundError as error:
-        print(ADD_DATA_WITH_FILENAME_ERROR.replace(r"{0}", str(error)))
-        # Print the stack trace
-        traceback.print_exc()
-
-    except AssertionError as error:
-        print(ADD_DATA_WITH_FILENAME_ERROR.replace(r"{0}", str(error)))
-        # Print the stack trace
-        traceback.print_exc()
-
-    finally:
-        # deletes file
-        TestHelpers.delete_file(data[ADD_DATA_WITH_FILENAME][SAVE_FILENAME])
+    # deletes file
+    TestHelpers.delete_file(data[ADD_DATA_WITH_FILENAME][SAVE_FILENAME])
 
 
 def test_add_data_to_empty_row_cell():
@@ -105,11 +78,6 @@ def test_add_data_to_empty_row_cell():
             data[ADD_DATA_EMPTY_ROW_CELL][SAVE_FILENAME],
             CURRENT_DIRECTORY + data[ADD_DATA_EMPTY_ROW_CELL][EXPECTED_OUTPUT_FILENAME],
         ) == (True, "Excel files are equal.")
-
-    except FileNotFoundError as error:
-        print(ADD_DATA_EMPTY_ROW_CELL_ERROR.replace(r"{0}", str(error)))
-        # Print the stack trace
-        traceback.print_exc()
 
     except AssertionError as error:
         print(ADD_DATA_EMPTY_ROW_CELL_ERROR.replace(r"{0}", str(error)))
