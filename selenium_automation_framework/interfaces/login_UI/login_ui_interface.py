6fe3914c7cb62918370eea5c97d0fec973aef1b6
Justine <justinevillarosa@gmail.com>
2024-02-26 10:26:10 +0800

    reconfigured login ui interface to integrate element setter to input xpath instead

--- selenium_automation_framework/interfaces/login_UI/login_ui_interface.py
+++ selenium_automation_framework/interfaces/login_UI/login_ui_interface.py
@@ -24,48 +24,6 @@ class LoginUIInterface:
         self._password_element: WebElement = None
         self._login_button_element: WebElement = None
 
-    @property
-    def username_xpath(self):
-        """_summary_: get method property for username xpath"""
-        return self._username_xpath
-
-    @username_xpath.setter
-    def username_xpath(self, xpath: str):
-        """_summary_: setter method property for username xpath
-
-        Args:
-            xpath (str): xpath of determiner to be used in getting username element
-        """
-        self._username_xpath = xpath
-
-    @property
-    def password_xpath(self):
-        """_summary_: get method property for password xpath"""
-        return self._password_xpath
-
-    @password_xpath.setter
-    def password_xpath(self, xpath: str):
-        """_summary_: setter method property for password xpath
-
-        Args:
-            xpath (str): xpath of determiner to be used in getting password element
-        """
-        self._password_xpath = xpath
-
-    @property
-    def login_button_xpath(self):
-        """_summary_: get method property for login button xpath"""
-        return self._login_button_xpath
-
-    @login_button_xpath.setter
-    def login_button_xpath(self, xpath: str):
-        """_summary_: setter method property for login button xpath
-
-        Args:
-            xpath (str): xpath of determiner to be used in getting login button
-        """
-        self._login_button_xpath = xpath
-
     ######################## WEBDRIVER ELEMENT PROPERTIES #############################
     @property
     def username_element(self):
@@ -75,14 +33,17 @@ class LoginUIInterface:
         return self._username_element
 
     @username_element.setter
-    def username_element(self, element: WebElement):
+    def username_element(self, xpath: str):
         """_summary_: setter method property for username element
 
         Args:
             element (_type_): element where element of username is stored
                 for caching.
         """
-        self._username_element = element
+        self._username_xpath = xpath
+        self._username_element = self.driver.find_element(
+            By.XPATH, self._username_xpath
+        )
 
     @property
     def password_element(self):
@@ -92,14 +53,17 @@ class LoginUIInterface:
         return self._password_element
 
     @password_element.setter
-    def password_element(self, element: WebElement):
+    def password_element(self, xpath: str):
         """_summary_: setter method property for password element
 
         Args:
             element (_type_): element where element of password is stored
                 for caching.
         """
-        self._password_element = element
+        self._password_xpath = xpath
+        self._password_element = self.driver.find_element(
+            By.XPATH, self._password_xpath
+        )
 
     @property
     def login_button_element(self):
@@ -109,11 +73,14 @@ class LoginUIInterface:
         return self._login_button_element
 
     @login_button_element.setter
-    def login_button_element(self, element: WebElement):
+    def login_button_element(self, xpath: str):
         """_summary_: setter method property for login button element
 
         Args:
             element (_type_): element where element of login button is stored
                 for caching.
         """
-        self._login_button_element = element
+        self._login_button_xpath = xpath
+        self._login_button_element = self.driver.find_element(
+            By.XPATH, self._login_button_xpath
+        )
