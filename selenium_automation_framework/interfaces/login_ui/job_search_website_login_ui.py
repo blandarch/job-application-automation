0208a544f14f14c01ef6998c583601714f255018
Justine <justinevillarosa@gmail.com>
2024-02-27 14:23:16 +0800

    Added interface class and intial ui xpath (to be changed later) for login for mock job website

---
+++ selenium_automation_framework/interfaces/login_ui/job_search_website_login_ui.py
@@ -0,0 +1,17 @@
+"""Imports Webdriver and LoginUIInterface class"""
+
+from selenium import webdriver
+from .login_ui_interface import LoginUIInterface
+
+
+class JobSearchWebsiteLoginUI(LoginUIInterface):
+    """_summary_: Inherits login_ui_interface to provide xpath and
+    elements to mock website Login UI"""
+
+    def __init__(self, driver: webdriver.Chrome):
+        super().__init__(
+            driver=driver,
+            username_xpath="username",
+            password_xpath="password",
+            login_buton_xpath="login",
+        )
