ecf80c9faaaf11d7eccf4904f216f06a6c2ee8c2
Justine <justinevillarosa@gmail.com>
2024-03-01 15:15:29 +0800

    added inherited class from UIJobSearchInterface initial

---
+++ selenium_automation_framework/interfaces/ui_job_search/ui_mock_website_job_search.py
@@ -0,0 +1,11 @@
+"""Imports Webdriver and Interface"""
+
+from selenium import webdriver
+from .ui_job_search_interface import UIJobSearchInterface
+
+
+class UIMockWebsiteJobSearch(UIJobSearchInterface):
+    """Inherited class for Mock Website Job Search UI"""
+
+    def __init__(self, driver: webdriver.Chrome):
+        super().__init__(driver, search_text_xpath="jobsearch")
