c69f1930399b1cadfad3c1260c553fa1597454e7
Justine <justinevillarosa@gmail.com>
2024-03-01 15:33:47 +0800

    Added initial logic to be able to do job search in the mock website

--- selenium_automation_framework/package/mock_job_website_actions.py
+++ selenium_automation_framework/package/mock_job_website_actions.py
@@ -1,6 +1,7 @@
 """Imports WebElement and use_determiner"""
 
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.keys import Keys
 from ..interfaces.switcher import (
     use_determiner,
 )
@@ -24,3 +25,16 @@ class MockJobWebsiteActions:
         self.properties.password_element.send_keys(password)
         # clicks login
         self.properties.login_button_element.click()
+
+    def search_jobs(self, search_text: str):
+        """_summary_: method to use when searching for jobs in the mock website
+
+        Args:
+            search_text (str): text to be inputed in the job search
+        """
+        # goes to the job search website for the mock website
+        self.properties.driver.get(self.properties.url)
+
+        # inputs job search and presses enter
+        self.properties.search_text_element.send_keys(search_text)
+        self.properties.search_text_element.send_keys(Keys.ENTER)
