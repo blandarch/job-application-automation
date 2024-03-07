62956b3aeaef7f55c66ed698d19f6afcb1c05858
Justine <justinevillarosa@gmail.com>
2024-03-07 13:23:19 +0800

    Added child class for mock website job results

---
+++ selenium_automation_framework/interfaces/ui_job_results/ui_job_results_mock_website.py
@@ -0,0 +1,17 @@
+"""Job Search UI Inheritance Class for Mock Website"""
+
+from selenium import webdriver
+
+from .ui_job_results_interface import UIJobResultsInterface
+
+
+class UIJobResultsMockWebsite(UIJobResultsInterface):
+    def __init__(self, driver: webdriver.Chrome):
+        super().__init__(
+            driver,
+            results_search_text_xpath='//input[@id="criterion271_27"]',
+            result_job_title_xpath="//h3/a",
+            result_job_description_xpath="Job Description",
+            result_job_company_xpath="""//p[@class="search-result-item-company-name"]
+                /span[class="companyName"]""",
+        )
