a69d6b67ee01607eeb42b66a116df0fee26d38b2
Justine <justinevillarosa@gmail.com>
2024-03-07 14:02:55 +0800

    added search results xpath to child class job results

--- selenium_automation_framework/interfaces/ui_job_results/ui_job_results_mock_website.py
+++ selenium_automation_framework/interfaces/ui_job_results/ui_job_results_mock_website.py
@@ -10,6 +10,7 @@ class UIJobResultsMockWebsite(UIJobResultsInterface):
         super().__init__(
             driver,
             results_search_text_xpath='//input[@id="criterion271_27"]',
+            search_results_xpath='//div[contains(@class, "s-res")]',
             result_job_title_xpath="//h3/a",
             result_job_description_xpath="Job Description",
             result_job_company_xpath="""//p[@class="search-result-item-company-name"]
