fe1cdb6ad200820464ecab65306f3b8e53606193
Justine <justinevillarosa@gmail.com>
2024-03-07 15:11:13 +0800

    replaced relative xpath to search bar

--- selenium_automation_framework/interfaces/ui_job_search/ui_mock_website_job_search.py
+++ selenium_automation_framework/interfaces/ui_job_search/ui_mock_website_job_search.py
@@ -8,4 +8,6 @@ class UIMockWebsiteJobSearch(UIJobSearchInterface):
     """Inherited class for Mock Website Job Search UI"""
 
     def __init__(self, driver: webdriver.Chrome):
-        super().__init__(driver, search_text_xpath='//input[@id="criterion271_35"]')
+        super().__init__(
+            driver, search_text_xpath='//input[contains(@id, "criterion271")]'
+        )
