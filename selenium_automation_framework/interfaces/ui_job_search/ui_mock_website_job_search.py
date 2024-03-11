f87546ecbed7a00baed3d8c0f84d3ac287b2734c
Justine <justinevillarosa@gmail.com>
2024-03-11 16:21:46 +0800

    transfered search button element only to mock website instance

--- selenium_automation_framework/interfaces/ui_job_search/ui_mock_website_job_search.py
+++ selenium_automation_framework/interfaces/ui_job_search/ui_mock_website_job_search.py
@@ -1,6 +1,8 @@
 """Imports Webdriver and Interface"""
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 from .ui_job_search_interface import UIJobSearchInterface
 
 
@@ -12,4 +14,27 @@ class UIMockWebsiteJobSearch(UIJobSearchInterface):
             driver,
             search_text_xpath='//input[contains(@id, "criterion271")]',
             search_button_xpath='//input[@type="button"][@value="Search Jobs"]',
+        )
+        self._search_button_xpath: str = (
+            '//input[@type="button"][@value="Search Jobs"]',
+        )
+        # Element Variables
+        self._search_button_element: WebElement = None
+
+    @property
+    def search_button_element(self) -> WebElement:
+        """_summary_: setter method property for search button element
+
+        Returns:
+            WebElement: web element for the search text
+        """
+        if self._search_button_element is None:
+            return self.driver.find_element(By.XPATH, self._search_button_xpath)
+        return self._search_button_element
+
+    @search_button_element.setter
+    def search_button_element(self, xpath: str):
+        self._search_button_xpath = xpath
+        self._search_button_element = self.driver.find_element(
+            By.XPATH, self._search_button_xpath
         )
