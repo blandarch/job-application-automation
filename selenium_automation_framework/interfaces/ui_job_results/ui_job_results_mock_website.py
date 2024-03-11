57e48b80e2826741415ec0ff53c79d8c32a25a01
Justine <justinevillarosa@gmail.com>
2024-03-11 15:36:52 +0800

    updated xpath fix to mock job website pytest

--- selenium_automation_framework/interfaces/ui_job_results/ui_job_results_mock_website.py
+++ selenium_automation_framework/interfaces/ui_job_results/ui_job_results_mock_website.py
@@ -9,10 +9,10 @@ class UIJobResultsMockWebsite(UIJobResultsInterface):
     def __init__(self, driver: webdriver.Chrome):
         super().__init__(
             driver,
-            results_search_text_xpath='//input[@id="criterion271_27"]',
+            results_search_text_xpath='//input[contains(@id, "criterion271")]',
             search_results_xpath='//div[contains(@class, "s-res")]',
             result_job_title_xpath="//h3/a",
-            result_job_description_xpath="Job Description",
-            result_job_company_xpath="""//p[@class="search-result-item-company-name"]
-                /span[class="companyName"]""",
+            result_job_description_xpath='//p[@class="search-result-item-description group"]',
+            result_job_company_xpath='//p[@class="search-result-item-company-name"]'
+            + '/span[@class="companyName"]',
         )
