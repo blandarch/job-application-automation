f1cb218fd264c0e09f91a69730b4d33936092044
Justine <justinevillarosa@gmail.com>
2024-03-20 14:38:29 +0800

    fixed xpath for job title

--- selenium_automation_framework/interfaces/ui_job_results/ui_job_results_linkedin.py
+++ selenium_automation_framework/interfaces/ui_job_results/ui_job_results_linkedin.py
@@ -20,7 +20,7 @@ class UIJobResultsLinkedIn(UIJobResultsInterface):
             driver,
             results_search_text_xpath='//input[contains(@id, "jobs-search-box-keyword-id")]',
             search_results_xpath='//li[contains(@id, "ember")][not(contains(@class, "artdeco"))]',
-            result_job_title_xpath='/h2[contains(@class, "job-title")]/a/span',
+            result_job_title_xpath='//h2[contains(@class, "job-title")]/a/span',
             result_job_description_xpath='//div[@id="job-details"]//span/p | //span/ul',
             result_job_company_xpath='//a[@class="app-aware-link "][contains(@href, "/company/")]',
         )
