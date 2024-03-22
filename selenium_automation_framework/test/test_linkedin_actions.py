7c5e65d03ebee9799942e7e3521deaf1231df5b3
Justine <justinevillarosa@gmail.com>
2024-03-22 10:51:03 +0800

    provided initial tests for search and store results (to be edited later)

--- selenium_automation_framework/test/test_linkedin_actions.py
+++ selenium_automation_framework/test/test_linkedin_actions.py
@@ -3,6 +3,7 @@
 from ..webdriver_setup.driver_setup import chromedriver_setup
 from ..package.linkedin_actions import LinkedInActions
 from .test_helpers import TestHelpers
+from ..objects.job_search_result import JobSearchResult
 
 
 def test_linkedin_login():
@@ -29,3 +30,22 @@ def test_ui_get_set():
         password_xpath='//label[contains(text(), "Password")]',
         login_button_xpath='//*[contains(text(), "Forgot password?")]',
     )
+
+
+def test_ui_job_search_and_store_result():
+    # sets up driver and linkedIn actions instance
+    linkedin_actions = LinkedInActions(chromedriver_setup())
+    # signs in and tests Job Search
+    linkedin_actions = TestHelpers.login_actions(linkedin_actions)
+    linkedin_actions = TestHelpers.ui_job_search(linkedin_actions, "QA Engineer")
+    # tests if it is able to produce job results and stores it in the object
+    job_results: list[JobSearchResult] = linkedin_actions.store_job_results()
+
+    # checks that items properties (except for date_posted) is not empty/None
+    for job_result in job_results:
+        assert len(job_result.job_title) != 0
+        assert len(job_result.job_description) != 0
+        assert len(job_result.company) != 0
+        assert job_result.date_posted is None
+
+    linkedin_actions.properties.driver.quit()
